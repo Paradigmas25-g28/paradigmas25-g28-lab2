15fc1db74a9e490033a0f3af8068e8ad5f687f1a
Constanza Buteler <cbuteler@mi.unc.edu.ar>
2025-05-20 16:14:43 -0300

    map finished

--- src/main/java/namedEntity/heuristic/Heuristic.java
+++ src/main/java/namedEntity/heuristic/Heuristic.java
@@ -11,6 +11,7 @@ import namedEntity.category.person.Title;
 import namedEntity.category.place.Ciudad;
 import namedEntity.category.place.Direction;
 import namedEntity.category.place.Pais;
+import namedEntity.category.place.Place;
 
 import java.util.Map;
 
@@ -19,27 +20,32 @@ public abstract class Heuristic {
   static Name n_musk = (new Name("elon", "hebrew", "ELON"));
   static LastName ln_musk = (new LastName("musk", "english"));
   static Title t_musk = (new Title("Phisics", true));
+  
   static Name n_messi = (new Name("lionel andres", "frances", "goat"));
   static LastName ln_messi = (new LastName("messi", "italiano"));
   static Title t_messi = (new Title("football player", false));
+  
   static Name n_biden = (new Name ("joseph robinette", "biblico", "JOE"));
   static LastName ln_biden = (new LastName ("biden", "irlandes"));
   static Title t_biden = (new Title ("Law", true));
+  
   static Name n_trump = (new Name ("donald","gaelic", "DONALD"));
   static LastName ln_trump = (new LastName("trump", "german"));
   static Title t_trump = (new Title("economist", true));
+  
   static Name n_federer = (new Name("roger", "german", "ROGER"));
   static LastName ln_federer = (new LastName("federer", "swiss"));
   static Title t_federer = (new Title("tennis player", false));
 
-  static Ciudad c_USA;
-  static Pais p_USA;
-  static Direction d_USA;
-  static Other o_USA;
-  static Ciudad c_Russia;
-  static Pais p_Russia;
-  static Direction d_Russia;
-  static Other o_Russia;
+  static Ciudad c_USA = (new Ciudad("Washington DC", 0, "United States of America"));
+  static Pais p_USA = (new Pais("english", 0));
+  static Direction d_USA = (null);
+  static Other o_USA = (null);
+  
+  static Ciudad c_Russia = (new Ciudad("Moscow", 0, "Russia"));
+  static Pais p_Russia = (new Pais("russian", 0));
+  static Direction d_Russia = (null);
+  static Other o_Russia = (null);
 
   public static Map<String, NamedEntity> categoryMap = Map.of(
       "Microsft", (new Company("Microsoft", "Other", 0, "microsoft", 0, "tech")),
@@ -50,8 +56,8 @@ public abstract class Heuristic {
       "Trump", (new Person("Trump", "International Politics", 0, "", n_trump, ln_trump, t_trump)),
       "Messi", (new Person("Messi", "Football", 0, "", n_messi, ln_messi, t_messi)),
       "Federer", (new Person("Federer", "Tennis", 0, "", n_federer, ln_federer, t_federer)),
-      "USA", "Country",
-      "Russia", "Country");
+      "USA", (new Place("USA","International Politics", 0, p_USA, c_USA, d_USA, o_USA)),
+      "Russia", (new Place("Russia","International Politics", 0, p_Russia, c_Russia, d_Russia, o_Russia)));
 
   // public String getCategory(String entity) {
   //   return categoryMap.get(entity);
