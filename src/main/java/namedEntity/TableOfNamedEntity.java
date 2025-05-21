0cc1a10af4fec75161a21e250f8487c5d1afdea9
Lautaro Famaf <lautaro.deco@mi.unc.edu.ar>
2025-05-20 21:11:25 -0300

    fix table

--- src/main/java/namedEntity/TableOfNamedEntity.java
+++ src/main/java/namedEntity/TableOfNamedEntity.java
@@ -18,188 +18,185 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 
-import javax.swing.plaf.basic.BasicTabbedPaneUI;
-
 import com.jakewharton.fliptables.FlipTable;
 
 public class TableOfNamedEntity {
 
-    List<Tripla> dataTableTopic;
-    List<Tripla> dataTableCategory;
-    Map<String, Integer> table;
+  List<Tripla> dataTableTopic;
+  List<Tripla> dataTableCategory;
+  Map<String, Integer> table;
 
-    public TableOfNamedEntity() {
-        this.dataTableTopic = new ArrayList<Tripla>();
-        this.dataTableCategory = new ArrayList<Tripla>();
-        this.table = new HashMap<String, Integer>();
+  public TableOfNamedEntity() {
+    this.dataTableTopic = new ArrayList<Tripla>();
+    this.dataTableCategory = new ArrayList<Tripla>();
+    this.table = new HashMap<String, Integer>();
+  }
+
+  private static List<String> ListKeys = Arrays.asList(
+      "NamedEntity",
+      "Person",
+      "Place",
+      "Other Category",
+      "Company",
+      "Product",
+      "Event",
+      "Date",
+      "Topic",
+      "Other Topic",
+      "Sports",
+      "Football",
+      "Basketball",
+      "Tennis",
+      "Formula1",
+      "Other Sport",
+      "Culture",
+      "Film",
+      "Music",
+      "Other Culture",
+      "Politics",
+      "National Politics",
+      "International Politics",
+      "Other Politics");
+
+  private static List<String> CatKeys = Arrays.asList(
+      "Person",
+      "Place",
+      "Company",
+      "Product",
+      "Event",
+      "Date",
+      "Other Category");
+
+  private static List<String> TopKeys = Arrays.asList(
+      "Sports",
+      "Football",
+      "Basketball",
+      "Tennis",
+      "Formula1",
+      "Other Sport",
+      "Culture",
+      "Film",
+      "Music",
+      "Other Culture",
+      "Politics",
+      "National Politics",
+      "International Politics",
+      "Other Politics",
+      "Other Topic");
+
+  public void InitialiceTable() {
+    for (String elem : ListKeys) {
+      table.put(elem, 0);
     }
+  }
 
-    private static List<String> ListKeys = Arrays.asList(
-            "NamedEntity",
-            "Person",
-            "Place",
-            "Other Category",
-            "Company",
-            "Product",
-            "Event",
-            "Date",
-            "Topic",
-            "Other Topic",
-            "Sports",
-            "Football",
-            "Basketball",
-            "Tennis",
-            "Formula1",
-            "Other Sport",
-            "Culture",
-            "Film",
-            "Music",
-            "Other Culture",
-            "Politics",
-            "National Politics",
-            "International Politics",
-            "Other Politics");
+  public void TheRealFunction(Feed feed, Heuristic hh) {
 
-    private static List<String> CatKeys = Arrays.asList(
-            "Person",
-            "Place",
-            "Company",
-            "Product",
-            "Event",
-            "Date",
-            "Other Category");
+    for (Article article : feed.getArticleList()) {
 
-    private static List<String> TopKeys = Arrays.asList(
-            "Sports",
-            "Football",
-            "Basketball",
-            "Tennis",
-            "Formula1",
-            "Other Sport",
-            "Culture",
-            "Film",
-            "Music",
-            "Other Culture",
-            "Politics",
-            "National Politics",
-            "International Politics",
-            "Other Politics",
-            "Other Topic"
-    );
+      article.computeNamedEntities(hh);
 
-    public void InitialiceTable() {
-        for (String elem : ListKeys) {
-            table.put(elem, 0);
+      List<NamedEntity> listEntities = article.getListArticles();
+
+      InitialiceTable();
+
+      table.put("NamedEntity", listEntities.size());
+
+      for (NamedEntity ne : listEntities) {
+        Integer frequency = ne.getFrequency();
+        NamedEntity nepapa = hh.getNamedEntity(ne.getName());
+        if (nepapa != null) {
+
+          String category = nepapa.getCategory();
+          String topic = nepapa.getTopic();
+          // Integer frequency = nepapa.getFrequency();
+          String name = nepapa.getName();
+
+          table.put(category, table.get(category) + frequency);
+          table.put(topic, table.get(topic) + frequency);
+
+          Tripla triplaNeT = new Tripla(name, category, frequency.toString());
+          Tripla triplaNeC = new Tripla(name, topic, frequency.toString());
+
+          dataTableCategory.add(triplaNeC);
+          dataTableTopic.add(triplaNeT);
+
+          System.err.println("NAME -> " + name);
+          System.err.println("FREQ -> " + frequency);
+          System.err.println(category);
+
+          System.err.println("Tabla[" + category + "] : "
+              + table.get(category).toString());
+          System.err.println(topic);
+          System.err.println("Tabla[" + topic + "] : " + table.get(topic).toString());
+          System.err.println("----------------------------------------------------------------------");
         }
+      }
     }
+  }
 
-    public void TheRealFunction(Feed feed, Heuristic hh) {
+  public void prettyPrint() {
+    String[] innerHeaders = { "One", "Two" };
+    String[][] innerData = { { "1", "2" } };
+    String inner = FlipTable.of(innerHeaders, innerData);
+    String[] headers = { "Left", "Right" };
+    String[][] data = { { inner, inner } };
+    System.out.println(FlipTable.of(headers, data));
+  }
 
-        for (Article article : feed.getArticleList()) {
+  // Funcion Tester
+  public static void main(String[] args) {
+    System.out.println("Compilo");
 
-            article.computeNamedEntities(hh);
+    QuickHeuristic qh = new QuickHeuristic();
+    List<Feed> feedl = new ArrayList<Feed>();
+    SubscriptionParser parser = new SubscriptionParser();
+    parser.parse("config/subscriptions.json");
+    httpRequester requester = new httpRequester();
 
-            List<NamedEntity> listEntities = article.getListArticles();
+    for (SingleSubscription subscription : parser.getSubscriptions()) {
+      if (subscription.getUrlType().equalsIgnoreCase("rss")) {
+        String urlFormat = subscription.getUrl();
 
-            InitialiceTable();
+        for (String topic : subscription.getUrlParams()) {
+          try {
+            String finalUrl = String.format(urlFormat, topic);
 
-            table.put("NamedEntity", listEntities.size());
+            System.out.println("Descargando RSS desde: " + finalUrl);
+            File rssXml = requester.getFeedRssToFile(finalUrl);
 
-            for (NamedEntity ne : listEntities) {
-                Integer frequency = ne.getFrequency();
-                NamedEntity nepapa = hh.getNamedEntity(ne.getName());
-                if (nepapa != null) {
+            RssParser rssParser = new RssParser();
+            rssParser.parse(rssXml.getPath());
 
-                    String category = nepapa.getCategory();
-                    String topic = nepapa.getTopic();
-                    // Integer frequency = nepapa.getFrequency();
-                    String name = nepapa.getName();
+            String siteName = subscription.getUrlType().toUpperCase() + " - " + topic;
 
-                    table.put(category, table.get(category) + frequency);
-                    table.put(topic, table.get(topic) + frequency);
+            Feed feed = new Feed(siteName);
 
-                    Tripla triplaNeT = new Tripla(name, category, frequency.toString());
-                    Tripla triplaNeC = new Tripla(name, topic, frequency.toString());
-
-                    dataTableCategory.add(triplaNeC);
-                    dataTableTopic.add(triplaNeT);
-
-                    System.err.println("NAME -> " + name);
-                    System.err.println("FREQ -> " + frequency);
-                    System.err.println(category);
-
-                    System.err.println("Tabla[" + category + "] : "
-                            + table.get(category).toString());
-                    System.err.println(topic);
-                    System.err.println("Tabla[" + topic + "] : " + table.get(topic).toString());
-                    System.err.println("----------------------------------------------------------------------");
-                }
+            for (Article article : rssParser.getArticles()) {
+              feed.addArticle(article);
             }
+            feedl.add(feed);
+
+          } catch (Exception e) {
+            System.err.println("Error al procesar el feed RSS para el topic: " + topic);
+            e.printStackTrace();
+          }
         }
+      }
     }
 
-    public static void prettyPrint() {
-        String[] innerHeaders = {"One", "Two"};
-        String[][] innerData = {{"1", "2"}};
-        String inner = FlipTable.of(innerHeaders, innerData);
-        String[] headers = {"Left", "Right"};
-        String[][] data = {{inner, inner}};
-        System.out.println(FlipTable.of(headers, data));
+    List<Feed> feedlpapa = feedl;
+    for (Feed feed : feedlpapa) {
+
+      TableOfNamedEntity tablita = new TableOfNamedEntity();
+      tablita.TheRealFunction(feed, qh);
+      tablita.prettyPrint();
+
+      // System.out.println("Tabla global de ocurrencias de entidades nombradas");
+      // tablita.prettyPrint("Global");
+      // System.out.println("Tabla de ocurrencias de categorias y topicos");
+      // tablita.prettyPrint("Class");
+      // System.out.println("Tabla de ocurrencias de categorias y topicos");
     }
-
-    // Funcion Tester
-    public static void main(String[] args) {
-        System.out.println("Compilo");
-
-        QuickHeuristic qh = new QuickHeuristic();
-        List<Feed> feedl = new ArrayList<Feed>();
-        SubscriptionParser parser = new SubscriptionParser();
-        parser.parse("config/subscriptions.json");
-        httpRequester requester = new httpRequester();
-
-        for (SingleSubscription subscription : parser.getSubscriptions()) {
-            if (subscription.getUrlType().equalsIgnoreCase("rss")) {
-                String urlFormat = subscription.getUrl();
-
-                for (String topic : subscription.getUrlParams()) {
-                    try {
-                        String finalUrl = String.format(urlFormat, topic);
-
-                        System.out.println("Descargando RSS desde: " + finalUrl);
-                        File rssXml = requester.getFeedRssToFile(finalUrl);
-
-                        RssParser rssParser = new RssParser();
-                        rssParser.parse(rssXml.getPath());
-
-                        String siteName = subscription.getUrlType().toUpperCase() + " - " + topic;
-
-                        Feed feed = new Feed(siteName);
-
-                        for (Article article : rssParser.getArticles()) {
-                            feed.addArticle(article);
-                        }
-                        feedl.add(feed);
-
-                    } catch (Exception e) {
-                        System.err.println("Error al procesar el feed RSS para el topic: " + topic);
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }
-
-        List<Feed> feedlpapa = feedl;
-        for (Feed feed : feedlpapa) {
-
-            TableOfNamedEntity tablita = new TableOfNamedEntity();
-            tablita.TheRealFunction(feed, qh);
-            tablita.prettyPrint();
-
-            // System.out.println("Tabla global de ocurrencias de entidades nombradas");
-            // tablita.prettyPrint("Global");
-            // System.out.println("Tabla de ocurrencias de categorias y topicos");
-            // tablita.prettyPrint("Class");
-            // System.out.println("Tabla de ocurrencias de categorias y topicos");
-        }
-    }
+  }
 }
